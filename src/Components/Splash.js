6537611db216dfb800c9276d469a68ecbb38c6ef
Spazcool <douglas.wrightiii@gmail.com>
2020-08-16 21:28:16 -0400

    add super basic animation to splash

--- src/Components/Splash.js
+++ src/Components/Splash.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import $ from 'jquery';
+import Grid from '@material-ui/core/Grid';
+import CardMedia from '@material-ui/core/CardMedia';
+import {icons} from '../data/icons.json';
 import '../App.css';
 
 export default function Splash(props){
+
+  useEffect(() => {
+    $('.hide').hide();
+    icons.forEach((icon, index) => {
+      $(`.${icon.name}`).delay(index * 1000).fadeIn(1000);
+    })
+  },[]);
+
+  const iconList = icons.map((icon) => <Grid item xs={3} className={`hide ${icon.name}`}>
+    <CardMedia
+      component="img"
+      alt={icon.name}
+      width='100%'
+      image={icon.url}
+    />
+  </Grid>);
+
   return (
-    <div>Splash</div>
+    <Grid container spacing={3} justify="center">
+      {iconList}
+    </Grid>
   )
 };
